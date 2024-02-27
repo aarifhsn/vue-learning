811c65bd26836e72d73dfd0245ffacf296e1139d
aarif <aarifhsn@gmail.com>
2024-02-27 13:30:26 +0600

    creating todo

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,17 +1,69 @@
 <script setup>
-import { ref, computed } from "vue";
+import { reactive } from "vue";
 
-const zip = ref("");
-
-const zipUpperCase = computed({
-  set: () => zip.value,
-  get: () => zip.value.toUpperCase(),
-});
+const todos = reactive([
+  {
+    id: 1,
+    title: "TODO ONE",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "TODO TWO",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "TODO THREE",
+    completed: true,
+  },
+]);
 </script>
 
 <template>
-  <input v-model="zip" type="text" name="name" id="" />
-  {{ zipUpperCase }}
+  <div
+    class="max-w-md mx-auto bg-white shadow-sm rounded overflow-hidden p-4 mt-6 space-y-6"
+  >
+    <h1 class="font-bold text-2xl my-2">Todo List (02/20)</h1>
+    <form>
+      <div class="flex border-1 border-slate-500 border-solid">
+        <input
+          class="border rounded w-full text-slate-700 p-2"
+          type="text"
+          placeholder="Add a task"
+        />
+        <button
+          class="border px-4 py-2 rounded hover:bg-slate-200"
+          type="submit"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+    <div class="todo_items mt-4">
+      <ul>
+        <li v-for="todo in todos" class="flex justify-between">
+          <div>
+            <input
+              :checked="todo.completed"
+              class="mr-2"
+              type="checkbox"
+              name="todo"
+              id="todo"
+            />
+            <label for="todo"
+              ><span>{{ todo.title }}</span></label
+            >
+          </div>
+          <div
+            class="delete px-2 py-1 border rounded font-bold hover:border-slate-300"
+          >
+            X
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
